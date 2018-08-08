0d10f92d2961b5f0e130f4a1064340650e5b030d
Dmitry Podlesny <rezed93@gmail.com>
2018-08-08 16:51:47 +0300

    svg types workaround

--- lib/templates/base/src/components/HomePage.tsx
+++ lib/templates/base/src/components/HomePage.tsx
@@ -4,6 +4,8 @@ import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 import * as links from 'utils/links'
 
+import SVGExample from './svg-example.svg'
+
 const Title = styled.div`
   margin: 60px 0;
   font-size: 18px;
@@ -17,6 +19,7 @@ export const HomePage = () => {
         <title>Gnarly Boilerplate</title>
       </Helmet>
       <Title>
+        <SVGExample width={120} height={120} />
         <div>Gnarly Boilerplate Here</div>
         <Link to={links.second}>Second Page</Link>
       </Title>

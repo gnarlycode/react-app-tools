ecd58dedcfa667479c213bf334b53a5c80efa9ec
rezed <rezed93@gmail.com>
2020-01-12 17:14:47 +0300

    small bugfixes

--- lib/templates/base/src/components/Common/Title.ts
+++ lib/templates/base/src/components/Common/Title.ts
@@ -1,6 +1,6 @@
 import styled from 'astroturf'
 
-const Title = styled.div`
+export const Title = styled.div`
   margin: 60px 0;
   font-size: 18px;
   text-align: center;
--- lib/templates/base/src/components/Layout/AppContainer.tsx
+++ lib/templates/base/src/components/Layout/AppContainer.tsx
@@ -4,13 +4,14 @@ import { RouteConfigComponentProps, renderRoutes } from 'react-router-config'
 import 'styles/globalStyles'
 
 // Typings
-interface AppContainerProps extends RouteConfigComponentProps<{}> {}
+type AppContainerProps = RouteConfigComponentProps
 
 // Component
 export const AppContainer = ({ route }: AppContainerProps) => {
   return <App>{route && renderRoutes(route.routes)}</App>
 }
 
+// Styles
 const App = styled.div`
   display: flex;
   flex-direction: column;
